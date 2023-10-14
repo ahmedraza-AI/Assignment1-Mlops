42bb2fc1806a09f39cc03f21292a4459e2c08909
ahmedraza-AI <111950355+ahmedraza-AI@users.noreply.github.com>
2023-10-14 20:30:28 +0000

    Update test.py

--- test.py
+++ test.py
@@ -5,23 +5,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import pytest
 
+@pytest.fixture
+def load_iris_model():
+    model = load_model('iris_model.h5')
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    
+    return X_test, y_test, model
+    
+def test_model_accuracy(load_iris_model):
+    X_test, y_test, model = load_iris_model
+    loss, accuracy = model.evaluate(X_test, y_test)
+    print(f'Test loss: {loss:.4f}, Test accuracy: {accuracy:.4f}')
+    assert accuracy >= 0.95, "The model is below the accuracy mark!"
 
-
-
-model = load_model('iris_model.h5')
-
-iris = load_iris()
-X, y = iris.data, iris.target
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-loss, accuracy = model.evaluate(X_test, y_test)
-print(f'Test loss: {loss:.4f}, Test accuracy: {accuracy:.4f}')
-
-np.savetxt("iris_training_data.csv", np.concatenate((X_train, y_train.reshape(-1, 1)), axis=1), delimiter=",")
-np.savetxt("iris_testing_data.csv", np.concatenate((X_test, y_test.reshape(-1, 1)), axis=1), delimiter=",")
